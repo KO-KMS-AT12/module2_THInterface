406228774802bd45b94d124fe2cd7194edfd81cb
ko <vtg0502@gmail.com>
2019-05-07 14:25:22 +0700

    thuc hanh interface

---
+++ Circle.java
@@ -0,0 +1,26 @@
+package shape;
+
+public class Circle {
+    private double radius;
+
+    public Circle() {
+        this.radius = 1.0;
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.pow(this.radius, 2) * Math.PI;
+    }
+
+}
---
+++ CircleComparator.java
@@ -0,0 +1,17 @@
+package shape;
+
+import java.util.Comparator;
+
+public class CircleComparator implements Comparator<Circle> {
+
+    @Override
+    public int compare(Circle c1, Circle c2) {
+        if (c1.getArea() > c2.getArea()) {
+            return 1;
+        } else if (c1.getArea() < c2.getArea()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
